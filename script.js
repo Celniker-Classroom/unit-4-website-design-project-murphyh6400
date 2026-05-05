2a22515ca56eacee178e08b5adeb119f96d369d2
murphyh6400 <murphyh6400@my.sduhsd.net>
2026-05-05 16:58:58 +0000

    2/3 javascript elements added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// add javascript here
+function readFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+function colorFunction() {
+  var element = document.body;
+  element.classList.toggle("orangemode");
+}
